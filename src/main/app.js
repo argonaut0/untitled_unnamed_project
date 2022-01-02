c58b58b099b0cff71faa3819a34d818bc3afdac1
Jason Hsu <jasonhsu2013@gmail.com>
2022-01-01 23:55:11 -0800

    Initial Commit

---
+++ src/main/app.js
@@ -0,0 +1,41 @@
+let blessed = require('blessed');
+
+let screen = blessed.screen({
+  smartCSR: true
+});
+
+screen.title = 'untitled_unnamed';
+
+// Create a box perfectly centered horizontally and vertically.
+let box = blessed.box({
+  top: 'center',
+  left: 'center',
+  width: '50%',
+  height: '50%',
+  content: 'Hello {bold}world{/bold}!',
+  tags: true,
+  border: {
+    type: 'line'
+  },
+  style: {
+    fg: 'white',
+    bg: 'magenta',
+    border: {
+      fg: '#f0f0f0'
+    }
+  }
+});
+
+// Append our box to the screen.
+screen.append(box);
+
+// Quit on Escape, q, or Control-C.
+screen.key(['escape', 'q', 'C-c'], function(ch, key) {
+  return process.exit(0);
+});
+
+// Focus our element.
+box.focus();
+
+// Render the screen.
+screen.render();
