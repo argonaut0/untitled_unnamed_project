12f89ca59e4eee73fa028f02952fbc126d643dc0
Jason Hsu <jasonhsu2013@gmail.com>
2022-01-03 22:47:46 -0800

    More random ui stuff

--- src/main/app.js
+++ src/main/app.js
@@ -1,7 +1,7 @@
 let blessed = require('blessed');
 let contrib = require('blessed-contrib');
 
-let screen = blessed.screen()
+let screen = blessed.screen();
 
 let grid = new contrib.grid({rows: 14, cols: 14, screen: screen});
 
@@ -16,9 +16,42 @@ let statdisp = grid.set(0, 0, 3, 5, contrib.donut,
     {label: 'MP', percent: 35, color: 'blue'},
     {label: 'STA', percent: 92, color: 'green'},
   ]
-})
+});
 
+let invdisp = grid.set(0, 5, 3, 6, contrib.table,
+  {
+    keys: true,
+    fg: 'white',
+    selectedFg: 'white',
+    selectedBg: 'blue',
+    interactive: true,
+    label: 'Inventory',
+    width: '30%',
+    height: '30%',
+    border: {type: "line", fg: "cyan"},
+    columnSpacing: 10,
+    columnWidth: [16, 12, 12] /*in chars*/
+  });
 
+invdisp.setData(
+  {
+    headers: ['Item', 'Weight', 'Count'],
+    data: 
+    [
+      ["Supplies", 1, 10],
+      ["Arrows", 3, 5]
+    ]
+  });
+
+invdisp.focus()
+
+let storylog = grid.set(3, 0, 5, 11, contrib.log,
+  {
+    fg: "green",
+    label: "Journal",
+  });
+
+const loremIpsum = require("lorem-ipsum").loremIpsum;
 
 
 screen.key(['escape', 'q', 'C-c'], function(ch, key) {
@@ -26,3 +59,9 @@ screen.key(['escape', 'q', 'C-c'], function(ch, key) {
 });
 
 screen.render();
+
+setInterval(logthings, 1000)
+
+function logthings() {
+  storylog.log(loremIpsum());
+}
